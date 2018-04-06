c613bdf8b5cb95d3dc51d081c7db4d2946e33d37
Abhi Ramani <abhiramani3@gmail.com>
2018-04-06 12:52:25 +0530

    Show cart if user is logedd in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'cart', to: 'cart#index', as: 'cart'
   resources :products do
   collection do
     get '/buy/:id', to: 'products#buy', as: 'buy'
   end
 end
   devise_for :users
-  root 'posts#index'
+  root 'products#index'
   resources 'posts'
 end
