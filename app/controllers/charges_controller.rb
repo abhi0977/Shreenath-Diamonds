792d7f4e881bf6d36ae9d3646f0fc04c2b9b30ba
Abhi Ramani <abhiramani3@gmail.com>
2018-04-06 23:23:15 +0530

    Add Checkout

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,28 @@
+class ChargesController < ApplicationController
+  def new
+    @tPrice = params[:tPrice]
+    p @tPrice
+  end
+
+def create
+  # Amount in cents
+  @tPrice = params[:tPrice].to_i
+  @amount = @tPrice * 100
+
+  customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
+  )
+
+  charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Rails Stripe customer',
+    :currency    => 'inr'
+  )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+end
